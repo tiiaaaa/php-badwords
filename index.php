10affdeb041d59cbfe313d5de0488018d5e8e349
MattiaPortanti <mattia.portantipm@gmail.com>
2022-04-19 16:40:57 +0200

    visulizzato il paragrafo

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+//Inizializzata variabile//
+$text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias accusantium itaque ducimus voluptates ad illum quod, magni necessitatibus assumenda ipsum, natus quibusdam quas ullam est eos atque tempora, architecto magnam!';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+    <div>
+        <h1>
+            <?php echo $text ?>
+        </h1>
+    </div>
+</body>
+</html>
